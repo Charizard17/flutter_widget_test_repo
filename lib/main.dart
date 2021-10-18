6f2d8eae635a580e0a10ac2247fb2981c8dbf78c
Esad Dursun <3723esad@gmail.com>
2021-10-18 09:26:32 +0200

    main widget/class created. app bar created

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,27 @@
 // below it
 // 2) Add a button which changes the text (to any other text of your choice)
 // 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test Title',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test App Bar Text'),
+        ),
+      ),
+    );
+  }
+}
