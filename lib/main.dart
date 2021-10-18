a7d7b1dd6ef432de2e5215fd149f3cb6d0c584df
Esad Dursun <3723esad@gmail.com>
2021-10-18 23:31:15 +0200

    text button added. button click changes the text

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,56 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
+
+  int initialIndex = 0;
+  void changeText() {
+    setState(() {
+      ++initialIndex;
+    });
+    print('button pressed');
+  }
+
+  String get showingText {
+    String textttt = initialIndex % 2 == 0
+        ? "This is a lorem ipsum text. I will change this text later."
+        : "This is the changed text!";
+    print(textttt);
+    return textttt;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test Title',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Test App Bar Text'),
+          title: Text('Flutter text changer'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(
+                showingText,
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              TextButton(
+                onPressed: changeText,
+                child: Text(
+                  'Change',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
