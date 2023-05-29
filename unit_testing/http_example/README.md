7d0ec308cf051411fb9d656a6c0d15910ca4a840
Charizard17 <3723esad@gmail.com>
2023-05-29 20:02:31 +0200

    example projects under unit_testing folder seperated

---
+++ unit_testing/http_example/README.md
@@ -0,0 +1,3 @@
+# flutter_widget_test_repo
+
+In this repo I test some widgets, new features or functions. Basicaly I use it just for practice purpose.
---
+++ unit_testing/http_example/android/app/src/main/kotlin/com/example/flutter_widget_test_repo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_widget_test_repo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
